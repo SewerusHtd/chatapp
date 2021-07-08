c6ea836c8b99293a845bf324ba31c8f41f2b56c2
sewerus <seweryn606@gmail.com>
2021-07-08 23:04:59 +0200

    action_cable auth

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,4 +1,8 @@
 class Message < ApplicationRecord
   belongs_to :user
   after_create_commit { MessageBroadcastJob.perform_later self}
+
+  def can_manage?(user)
+    user.id == self.user_id or user.is_a? Admin
+  end
 end
